d7b39ff8c3495ff4bee7e3aa84c997b34051d9c8
Nam Minjeong <nmj5012@naver.com>
2022-04-27 15:01:16 +0900

    feat: 체스방 생성, 리스트 보여주기 기능 추가

---
+++ src/main/java/chess/dto/BoardInfoDto.java
@@ -0,0 +1,11 @@
+package chess.dto;
+
+public class BoardInfoDto {
+    private final int id;
+    private final String name;
+
+    public BoardInfoDto(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
